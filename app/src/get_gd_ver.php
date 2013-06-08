d3a8b3c56437485f44b6d494cfc2a6f690a71923
futoase <futoase@gmail.com>
2013-06-09 00:28:41 +0900

    Modified: Prettify of get_gd_ver.php

--- app/src/get_gd_ver.php
+++ app/src/get_gd_ver.php
@@ -1,10 +1,15 @@
 <?php
-//gdのバージョンを調べる
+/**
+ * Get GD Version.
+ *
+ * @return string GD Information.
+ */
 function get_gd_ver(){
   if(function_exists("gd_info")){
     $gdver=gd_info();
     $phpinfo=$gdver["GD Version"];
-  }else{ //php4.3.0未満用
+  }
+  else{ //php4.3.0未満用
     ob_start();
     phpinfo(8);
     $phpinfo=ob_get_contents();
@@ -13,6 +18,7 @@ function get_gd_ver(){
     $phpinfo=stristr($phpinfo,"gd version");
     $phpinfo=stristr($phpinfo,"version");
   }
+
   $end=strpos($phpinfo,".");
   $phpinfo=substr($phpinfo,0,$end);
   $length = strlen($phpinfo)-1;
