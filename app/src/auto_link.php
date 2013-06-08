a2b5d11b57e8b9d59d71dd88cbf8faa21cec25bd
futoase <futoase@gmail.com>
2013-06-08 13:29:31 +0900

    Modified: Prettify of auto_link.php

--- app/src/auto_link.php
+++ app/src/auto_link.php
@@ -1,7 +1,15 @@
 <?php
-/* オートリンク */
-function auto_link($proto){
-  $proto = preg_replace("/(https?|ftp|news)(:\/\/[[:alnum:]\+\$\;\?\.%,!#~*\/:@&=_-]+)/","<a href=\"\\1\\2\" target=\"_blank\">\\1\\2</a>",$proto);
-  return $proto;
+/**
+ * Create http link.
+ *
+ * @params string $message message.
+ * @return string Replaced to the link.
+ */
+function auto_link($message){
+  return preg_replace(
+    "/(https?|ftp|news)(:\/\/[[:alnum:]\+\$\;\?\.%,!#~*\/:@&=_-]+)/",
+    "<a href=\"\\1\\2\" target=\"_blank\">\\1\\2</a>",
+    $message
+  );
 }
 ?>
