dc3c7ec47b2f53ecf092039b4756fbdfeb00cd4c
futoase <futoase@gmail.com>
2013-06-09 00:55:18 +0900

    Modified: Prettify of main.php

--- app/src/main.php
+++ app/src/main.php
@@ -1,17 +1,21 @@
 <?php
-/*-----------Main-------------*/
 init();		//←■■初期設定後は不要なので削除可■■
 $iniv=array('mode','name','email','sub','com','pwd','upfile','upfile_name','resto','pass','res','post','no');
 foreach($iniv as $iniva){
-  if(!isset($$iniva)){$$iniva="";}
+  if(!isset($$iniva)){
+    $$iniva="";
+  }
 }
+
 switch($mode){
   case 'regist':
     regist($name,$email,$sub,$com,'',$pwd,$upfile,$upfile_name,$resto);
     break;
   case 'admin':
     valid($pass);
-    if($admin=="del") admindel($pass);
+    if($admin=="del"){
+      admindel($pass);
+    }
     if($admin=="post"){
       echo "</form>";
       form($post,$res,1);
