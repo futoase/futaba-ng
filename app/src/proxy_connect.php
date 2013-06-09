8fcc3fadc3dc38b4edb8613cf77d16ef4f56daaa
futoase <futoase@gmail.com>
2013-06-09 10:03:35 +0900

    Modified: Prettify of proxy_connect.php

--- app/src/proxy_connect.php
+++ app/src/proxy_connect.php
@@ -1,7 +1,19 @@
 <?php
-function  proxy_connect($port) {
+/**
+ * Connect to port with reverse proxy. 
+ * 
+ * @params integer $port target port.
+ * @return integer 1 then success.
+ *         integer 0 then error.
+ */
+function proxy_connect($port){
   $a="";$b="";
-  $fp = @fsockopen ($_SERVER["REMOTE_ADDR"], $port,$a,$b,2);
-  if(!$fp){return 0;}else{return 1;}
+  $fp = @fsockopen($_SERVER["REMOTE_ADDR"], $port,$a,$b,2);
+  if(!$fp){
+    return 0;
+  }
+  else{
+    return 1;
+  }
 }
 ?>
